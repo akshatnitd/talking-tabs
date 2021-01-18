f460b0f360577e0d275586c52fbdf74bf6b94ce8
Akshat Garg <akshat.garg@uber.com>
2021-01-19 00:03:24 +0530

    Add shared worker

---
+++ assets/js/sharedWorker.js
@@ -0,0 +1,41 @@
+
+
+let ports = []
+
+onconnect = function (e) {
+
+  const port = e.ports[0];
+
+  ports.push(port)
+
+  port.onmessage = function (event) {
+
+    console.log("Recieved Message: ", event.data)
+
+    if (event.data === "HI") {
+      this.postMessage("HI From Worker")
+      return
+    }
+
+    if (event.data === "HELLO") {
+      ports.forEach(portInst => portInst.postMessage("HELLO From Worker"))
+      return
+    }
+
+    if (event.data === "BONJOUR") {
+      ports.forEach((portInst, index) => {
+        if (index % 2 === 0) { return }
+        portInst.postMessage("BONJOUR From Worker")
+      })
+      return
+    }
+
+  }
+}
+
+function broadcast(message) {
+  ports.forEach(portInst => portInst.postMessage(message))
+}
+
+
+
